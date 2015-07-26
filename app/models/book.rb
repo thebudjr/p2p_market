10a0f2023abe36c2a4153040397a7fbcb398e43b
thebudjr <tim.s.janney@gmail.com>
2015-07-26 18:06:48 -0400

    added sales model

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,15 +4,19 @@ class Book < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image
 	has_attached_file :resource
+	has_many :sales
 
 	validates_attachment_content_type :image, 
 content_type:  /^image\/(png|gif|jpeg)/,
 message: "Only png, gif, and jpeg images allowed"
 
-validates_attachment_content_type :resource,
+	validates_attachment_content_type :resource,
 content_type: ['application/pdf'],
 message: "Only pdfs allowed"
 
-validates :image, attachment_presence: true
-validates :resource, attachment_presence: true
+	validates :image, attachment_presence: true
+	validates :resource, attachment_presence: true
+
+	validates_numericality_of :price,
+	greater_than: 49, message: "Must be at least 50 cents"
 end
